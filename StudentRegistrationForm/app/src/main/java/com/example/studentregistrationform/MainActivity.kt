33b19c71265e28969baf5a20d6ee14b760bd627d
Nisal Nimsara <KUDSE232F-048@student.nibm.lk>
2025-05-17 10:53:15 +0530

    Updated Seller Registration

--- StudentRegistrationForm/app/src/main/java/com/example/studentregistrationform/MainActivity.kt
+++ StudentRegistrationForm/app/src/main/java/com/example/studentregistrationform/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.example.studentregistrationform
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val name = findViewById<EditText>(R.id.editName)
+        val email = findViewById<EditText>(R.id.editEmail)
+        val phone = findViewById<EditText>(R.id.editPhone)
+        val dob = findViewById<EditText>(R.id.editDob)
+        val genderGroup =  findViewById<RadioGroup>(R.id.genderGroup)
+
+        val subminBtn = findViewById<Button>(R.id.btnSubmit)
+
+        subminBtn.setOnClickListener{
+            val selectedGenderId = genderGroup.checkedRadioButtonId
+            val gender = findViewById<RadioButton>(selectedGenderId)?.text ?: "Not Selected"
+
+            val message = """
+                Name: ${name.text}
+                Email: ${email.text}
+                Phone: ${phone.text}
+                DOB: ${dob.text}
+                Gender: $gender
+            """.trimIndent()
+
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
         }
     }
 }
