b2364c3dd129ac608513f2f25c0ac68b3c906447
Nisal Nimsara <KUDSE232F-048@student.nibm.lk>
2025-05-23 15:25:57 +0530

    Add image buttons to MainActivity for displaying selected images

--- SimpleGalleryApp/app/src/main/java/com/example/simplegalleryapp/MainActivity.kt
+++ SimpleGalleryApp/app/src/main/java/com/example/simplegalleryapp/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.simplegalleryapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var imageView : ImageView
+    lateinit var buttonImage1: Button
+    lateinit var buttonImage2: Button
+    lateinit var buttonImage3: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        imageView = findViewById(R.id.imageView)
+        buttonImage1 = findViewById(R.id.buttonImage1)
+        buttonImage2 = findViewById(R.id.buttonImage2)
+        buttonImage3 = findViewById(R.id.buttonImage3)
+
+        buttonImage1.setOnClickListener {
+            imageView.setImageResource(R.drawable.sigiriya)
+        }
+
+        buttonImage2.setOnClickListener {
+            imageView.setImageResource(R.drawable.galle)
+        }
+
+        buttonImage3.setOnClickListener {
+            imageView.setImageResource(R.drawable.kandy)
         }
     }
 }
