21e8c6bc5fc15f810d3ba7e4bfe311803ffd8d52
Nisal Nimsara <KUDSE232F-048@student.nibm.lk>
2025-05-23 11:10:18 +0530

    Refactor layout and implement explicit intent functionality

--- ExplicitIntentDemo/app/src/main/java/com/example/explicitintentdemo/MainActivity.kt
+++ ExplicitIntentDemo/app/src/main/java/com/example/explicitintentdemo/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.explicitintentdemo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,16 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val editText = findViewById<EditText>(R.id.editTextName)
+        val button = findViewById<Button>(R.id.buttonSend)
+
+        button.setOnClickListener {
+            val name = editText.text.toString()
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("user_name", name)
+            startActivity(intent)
         }
     }
 }
--- ExplicitIntentDemo/app/src/main/java/com/example/explicitintentdemo/SecondActivity.kt
+++ ExplicitIntentDemo/app/src/main/java/com/example/explicitintentdemo/SecondActivity.kt
@@ -11,7 +11,7 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_second)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layoutSecond)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
