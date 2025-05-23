ce1542c16939bf4eaae5bdfec80c0ac1534cded3
Nisal Nimsara <KUDSE232F-048@student.nibm.lk>
2025-05-23 11:46:27 +0530

    Add ThirdActivity and navigation from SecondActivity with confirmation button

--- ExplicitIntentDemo/app/src/main/java/com/example/explicitintentdemo/SecondActivity.kt
+++ ExplicitIntentDemo/app/src/main/java/com/example/explicitintentdemo/SecondActivity.kt
@@ -1,20 +1,31 @@
 package com.example.explicitintentdemo
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SecondActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_second)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layoutSecond)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val textView = findViewById<TextView>(R.id.textViewResult)
+        val name = intent.getStringExtra("user_name")
+        textView.text = "Welcome, $name!"
+
+        val button = findViewById<Button>(R.id.buttonThanks)
+
+        button.setOnClickListener {
+            val intent = Intent(this, ThirdActivity::class.java)
+            startActivity(intent)
         }
+
     }
 }
---
+++ ExplicitIntentDemo/app/src/main/java/com/example/explicitintentdemo/ThirdActivity.kt
@@ -0,0 +1,21 @@
+package com.example.explicitintentdemo
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ThirdActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_third)
+
+        val textView = findViewById<TextView>(R.id.textViewThankYou)
+        textView.text = "Thank You!"
+    }
+}
