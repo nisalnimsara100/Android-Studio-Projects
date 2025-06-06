3a085b9322c67ea31a2b47fc6a5023423e8a5cf7
Nisal Nimsara <KUDSE232F-048@student.nibm.lk>
2025-06-06 09:59:48 +0530

    Implement ListView with ArrayAdapter and item click listener in MainActivity

--- ListViewwithArrayAdapter/app/src/main/java/com/example/listviewwitharrayadapter/MainActivity.kt
+++ ListViewwithArrayAdapter/app/src/main/java/com/example/listviewwitharrayadapter/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.listviewwitharrayadapter
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,5 +17,21 @@ class MainActivity : AppCompatActivity() {
         "Sri Lanka", "India", "USA", "Canada", "Australia", "Germany",
         "France", "Italy", "Spain", "Japan", "China"
     )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        listView = findViewById(R.id.listViewCountries)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, countries)
+
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener { _, _, position, _ ->
+            val selectedCountry = countries[position]
+            Toast.makeText(this, "You Selected: $selectedCountry", Toast.LENGTH_SHORT).show()
+        }
+    }
 
 }
