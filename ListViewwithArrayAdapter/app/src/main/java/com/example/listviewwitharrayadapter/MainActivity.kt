b03c3ccd5a95408e2e20047390470c768f3f0760
Nisal Nimsara <KUDSE232F-048@student.nibm.lk>
2025-06-06 09:52:13 +0530

    Refactor MainActivity to initialize ListView and countries array

--- ListViewwithArrayAdapter/app/src/main/java/com/example/listviewwitharrayadapter/MainActivity.kt
+++ ListViewwithArrayAdapter/app/src/main/java/com/example/listviewwitharrayadapter/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.example.listviewwitharrayadapter
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+
+    private lateinit var listView: ListView
+    private val countries = arrayOf(
+        "Sri Lanka", "India", "USA", "Canada", "Australia", "Germany",
+        "France", "Italy", "Spain", "Japan", "China"
+    )
+
 }
